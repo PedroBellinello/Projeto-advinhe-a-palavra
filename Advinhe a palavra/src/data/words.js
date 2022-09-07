f99c5d20fb77074a164e0e894a791270b1f9a519
PedroBellinello <107586888+PedroBellinello@users.noreply.github.com>
2022-09-07 20:54:02 -0300

    Add files via upload
    Primeiro projeto utilizando React

---
+++ Advinhe a palavra/src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "Radiador"],
+    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Morango", "uva"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos", "Cotovelo"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+    programação: ["Linguagem", "Framework", "JavaScript", "React", "Desenvoldedor"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+  };
